c333bf82e1feddfb51daabadb507abc4839b535e
1ighter <zjplighter@icloud.com>
2023-12-12 20:03:51 +0800

    Update clippy.py

--- clippy.py
+++ clippy.py
@@ -3,9 +3,9 @@ import tensorflow as tf
 
 class ClippyAdagrad(tf.train.Optimizer):
 
-    def __init__(self, learning_rate=0.001, initial_accumulator_value=0.1,
-                 variable_relative_threshold=0.1, accumulator_relative_threshold=0.0,
-                 absolute_threshold=1e-7, epsilon=1e-7, name='ClippyAdagrad', use_locking=False):
+    def __init__(self, learning_rate=0.1, initial_accumulator_value=0.0,
+                 variable_relative_threshold=0.5, accumulator_relative_threshold=0.0,
+                 absolute_threshold=0.01, epsilon=1e-7, name='ClippyAdagrad', use_locking=False):
 
         super(ClippyAdagrad, self).__init__(name=name, use_locking=False)
         self._lr = learning_rate
@@ -19,6 +19,7 @@ class ClippyAdagrad(tf.train.Optimizer):
 
     def apply_gradients(self, grads_and_vars, global_step=None, name=None):
 
+        var_updates = []
         for (grad, var) in grads_and_vars:
 
             if var not in self._accumulators:
@@ -36,25 +37,36 @@ class ClippyAdagrad(tf.train.Optimizer):
             precondition = tf.rsqrt(accumulator_values + epsilon)
             delta = lr * grad * precondition
 
-            clipped_delta, _ = self._clip_by_thresholds(delta, var, precondition)
+            clipped_delta, _ = self.shrink_by_references(delta, [var, precondition],
+                                                         [self._var_relative_threshold,
+                                                          self._accumulator_relative_threshold],
+                                                         self._absolute_threshold)
 
             var_update = var.assign_sub(clipped_delta)
+            var_updates.append(var_update)
 
-        return tf.group(*[var_update for var_update, _ in grads_and_vars])
+        if global_step is not None:
+            with tf.control_dependencies(var_updates):
+                global_step_update = tf.assign_add(global_step, 1)
+                var_updates.append(global_step_update)
 
-    def _clip_by_thresholds(self, grad, var, precondition):
+        return tf.group(*var_updates, name=name)
 
-        references = [var, precondition]
-        relative_factors = [self._var_relative_threshold, self._accumulator_relative_threshold]
+    def shrink_by_references(self, tensor, references, relative_factors, absolute_factor):
+        if any(relative_factor < 0 for relative_factor in relative_factors):
+            raise ValueError("relative_factors must all be non-negative.")
+        if absolute_factor < 0:
+            raise ValueError("absolute_factor must be non-negative.")
+        if len(references) != len(relative_factors):
+            raise ValueError("References and relative_factors must have the same length.")
 
-        max_norm = self._absolute_threshold
+        max_norm = absolute_factor
         for ref, relative_factor in zip(references, relative_factors):
-            max_norm += relative_factor * tf.reduce_max(tf.abs(ref))
+            max_norm += relative_factor * tf.abs(ref)
 
-        grad_norm = tf.norm(grad)
-        return tf.where(tf.greater(grad_norm, max_norm),
-                        grad * (max_norm / grad_norm),
-                        grad), max_norm
+        scale = tf.minimum(1.0, max_norm / tf.reduce_max(tf.abs(tensor)))
+        shrinked_tensor = tensor * scale
+        return tensor * scale, scale
 
     def _apply_dense(self, grad, var):
         raise NotImplementedError
